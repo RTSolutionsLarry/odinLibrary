4abf00b297be39187bc7b23f2a0553db36a6b46f
RTSolutionsLarry <larry.randolph@randolphtechnologysolutions.com>
2025-10-31 13:12:02 -0700

    Add cards when button is clicked

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const bookName = document.getElementById('bookName');
 const authorName = document.getElementById('authorName');
 const numberOfPages = document.getElementById('numberOfPages');
 const genreName = document.getElementById('genreName');
+const cardsContainer = document.getElementsByClassName('cardsContainer')[0];
 const library = [];
 
 //this is the book constructor
@@ -20,8 +21,8 @@ Book.prototype.bookIdGen = function() {
     this.bookId = idGen;
 }
 
-function addBook(name) {
-    const newBook = new Book(name);
+function addBook(name,author,numberOfPages,genre,hasBeenRead) {
+    const newBook = new Book(name,author,numberOfPages,genre,hasBeenRead);
     newBook.bookIdGen();
     library.push(newBook);
 }
@@ -30,17 +31,23 @@ addBook('Harry Potter');
 
 const displayBook = (library) => {
     for (let book of library) {
-        console.log(book);      
+        const card = document.createElement('div');
+        card.setAttribute('class','card');
+        cardsContainer.appendChild(card);      
     }
 }
 
-displayBook(library);
-
 //Add Book button
 addBookButton.addEventListener('click', ()=> {
+    //test code to check inputs
     console.log('You clicked!');
     console.log(bookName.value);
     console.log(authorName.value);
     console.log(numberOfPages.value);
     console.log(genreName.value);
+
+    addBook(bookName.value,authorName.value,numberOfPages.value,genreName.value,false);
+    console.table(library);
+    displayBook(library);
 })
+
